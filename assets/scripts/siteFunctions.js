94909f56a6fb8eca9e1b7958bb0c34f5968412be
cantonyrodri <carodriguez@unitru.edu.pe>
2024-08-25 23:30:58 -0500

    Agregando IDs en HTML y clases adicionales para funciones en JS

---
+++ assets/scripts/siteFunctions.js
@@ -0,0 +1,21 @@
+/* Reglas de encriptado
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
+
+text-input
+instruction-message
+btn-encryptor
+btn-decryptor
+main-right
+muneco
+text-output
+// .buttons__decryptorAlteration ->Cambia el color cuando se habilita
+// .main__rightReplace ->justify content space between
+// .main__right__textAlteration ->Cuando se va a encriptar
+// .hide__element ->Clase para ocultar
+
